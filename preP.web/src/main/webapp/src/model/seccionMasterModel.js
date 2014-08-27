a4bace6533bedccc2cc9a13686510b5570267af8
pacho950609 <franciscoricaurte-95@hotmail.com>
2014-08-26 23:33:19 -0500

    Mi Parte :) ... pacho

--- preP.web/src/main/webapp/src/model/seccionMasterModel.js
+++ preP.web/src/main/webapp/src/model/seccionMasterModel.js
@@ -1,11 +1,30 @@
 define(['model/_seccionMasterModel'], function() { 
     App.Model.SeccionMasterModel = App.Model._SeccionMasterModel.extend({
+        
+        
+        
+       initialize: function() {
+            this.on('invalid', function(model,error) {
+                Backbone.trigger('seccion-master-model-error', error);
+            });
+        },
+        validate: function(attrs, options){
+            var modelMaster = new App.Model.SeccionModel();
+            if(modelMaster.funcionara){
+            	return modelMaster.funcionara(attrs.seccionEntity,options);
+            }
+            
+            
+            
+        }
+   
 
     });
 
     App.Model.SeccionMasterList = App.Model._SeccionMasterList.extend({
         model: App.Model.SeccionMasterModel
     });
+    
 
     return  App.Model.SeccionMasterModel;
 
